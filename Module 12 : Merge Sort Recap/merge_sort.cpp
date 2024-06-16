7aa68f33479556ffe0b50520347ef478d2cd4242
Md. Uzzal Mia <129536126+uzzal2200@users.noreply.github.com>
2024-06-16 20:28:26 +0600

    Add files via upload

---
+++ Module 12 : Merge Sort Recap/merge_sort.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+void merge(int a[],int l,int m,int r)
+{
+    int leftsize=m-l+1;
+    int rightsize=r-m;
+    int L[leftsize],R[rightsize];
+    int k=0;
+    for(int i=l;i<=m;i++)
+    {
+        L[k]=a[i];
+        k++;
+    }
+    k=0;
+    for(int i=m+1;i<=r;i++)
+    {
+        R[k]=a[i];
+        k++;
+    }
+   int i=0,j=0;
+   int cur=l;
+   while(i < leftsize && j < rightsize)
+   {
+    if(L[i] <= R[j])
+    {
+        a[cur]=L[i];
+        i++;
+    }
+    else
+    {
+    a[cur]=R[j];
+    j++;
+    }
+     cur++;
+   }
+   while(i < leftsize)
+   {
+    a[cur]=L[i];
+    i++;
+    cur++;
+   }
+   while(j<rightsize)
+   {
+    a[cur]=R[j];
+    j++;
+    cur++;
+   }
+}
+void merge_sort(int a[],int l,int r)
+{
+     if(l<r)
+    {
+        int mid=(l+r)/2;
+        merge_sort(a,l,mid);// dui bag korar jonno complexity logn
+        merge_sort(a,mid+1,r);
+        merge(a,l,mid,r); // O(n)
+        // cout<<"This"<<endl;
+        // for(int i=l;i<=mid;i++)
+        // {
+        //     cout<<a[i]<<" ";
+        // }
+        // cout<<endl;
+        // for(int i=mid+1;i<=r;i++)
+        // {
+        //     cout<<a[i]<<" ";
+        // }
+        // cout<<endl;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+     int a[n];
+     for(int i=0;i<n;i++)
+     {
+        cin>>a[i];
+     }
+      merge_sort(a,0,n-1);
+     for(int i=0;i<n;i++)
+     {
+        cout<<a[i]<<" ";
+     }
+    return 0;
+}
